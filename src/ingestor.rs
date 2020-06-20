c722abfb1aaa4334cdad97d51c70b4e750a08c7f
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-06-20 18:34:38 +0530

    Move tasks-spawning into an Ingestor struct

---
+++ src/ingestor.rs
@@ -0,0 +1,23 @@
+use crate::rotator::JPEGRotator;
+use async_std::task;
+use futures::future::join_all;
+use std::io::Error;
+pub struct Ingestor;
+
+impl Ingestor {
+    pub async fn start(files: Vec<String>) {
+        let mut futures_list = vec![];
+        for path in files {
+            futures_list.push(task::spawn(async {
+                let rotator = JPEGRotator::new(path)?;
+                if let Err(e) = rotator.rotate().await {
+                    println!("an error occurred - {}", e);
+                }
+                Ok::<(), Error>(())
+            }));
+        }
+
+        println!("waiting for all rotations to complete ...");
+        join_all(futures_list).await;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,16 @@
 extern crate async_std;
+
+pub mod ingestor;
 pub mod rotator;
 pub mod utils;
 
-use async_std::task;
-use futures::future::join_all;
-use rotator::JPEGRotator;
+use ingestor::Ingestor;
 use std::env;
-use std::io::Error;
 use std::time::Instant;
 
 #[async_std::main]
 async fn main() {
-    let mut futures_list = vec![];
     let now = Instant::now();
-    for path in env::args().skip(1) {
-        futures_list.push(task::spawn(async move {
-            let rotator = JPEGRotator::new(path)?;
-            if let Err(e) = rotator.rotate().await {
-                println!("an error occurred - {}", e);
-            }
-            Ok::<(), Error>(())
-        }));
-    }
-
-    println!("waiting for all rotations to complete ...");
-    join_all(futures_list).await;
-    println!("time taken - {}", now.elapsed().as_secs());
+    Ingestor::start(env::args().skip(1).collect()).await;
+    println!("time taken - {} secs", now.elapsed().as_secs());
 }
