666d50f43d6bcac80a2936da9183d5cda58422e9
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-06-20 16:39:28 +0530

    Encapsulate rotator logic into a JPEGRotator struct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate async_std;
 pub mod rotator;
 use async_std::task;
 use futures::future::join_all;
+use rotator::JPEGRotator;
 use std::env;
 use std::time::Instant;
 
@@ -11,7 +12,7 @@ async fn main() {
     let now = Instant::now();
     for path in env::args().skip(1) {
         futures_list.push(task::spawn(async move {
-            rotator::run_rotation(path.clone()).await;
+            JPEGRotator::run_rotation(path.clone()).await;
         }));
     }
 
