11e57d0759fe0fa81b37372bbeba033601149e8b
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-06-20 18:31:11 +0530

    Separate out rotation into a separate method and enhance logging

--- src/rotator.rs
+++ src/rotator.rs
@@ -4,11 +4,8 @@ extern crate image;
 use crate::utils;
 use exif::{Exif, In, Tag, Value};
 use image::{DynamicImage, ImageFormat};
+use std::fs::{File, OpenOptions};
 use std::io::{Error, Seek};
-use std::{
-    fs::{File, OpenOptions},
-    path::Path,
-};
 
 trait Rotator {
     fn rotate(&mut self);
@@ -20,55 +17,67 @@ pub struct JPEGRotator {
 }
 
 impl JPEGRotator {
+    /// Returns a new JPEG rotator if the image file is successfully opened.
+    /// Otherwise, an Error
     pub fn new(path: String) -> Result<Self, Error> {
         let file = OpenOptions::new().read(true).write(true).open(&path)?;
 
         Ok(JPEGRotator { path, file })
     }
 
+    /// Performs rotation on the internal image handle depending on
+    /// the orientation value.
     pub async fn rotate(&self) -> Result<(), String> {
+        println!("processing file: {}", self.path);
         let exif = utils::get_exif(&self.file)?;
         let orientation = utils::get_orientation_value(&exif);
         println!("orientation is {}", orientation);
 
-        let bufreader = std::io::BufReader::new(&self.file);
-
-        let rotated_image = match image::load(bufreader, ImageFormat::Jpeg) {
-            Ok(image) => match orientation {
-                1 | 2 | 4 | 5 | 7 => {
-                    println!("orientation looks cool already!");
-                    None
-                }
-                8 => {
-                    println!("rotating 270 degrees ...");
-                    Some(image.rotate270())
-                }
-                3 => {
-                    println!("rotating 180 degrees ...");
-                    Some(image.rotate180())
-                }
-                6 => {
-                    println!("rotating 90 degrees ...");
-                    Some(image.rotate90())
-                }
-                _ => {
-                    println!("invalid orientation found :(");
-                    None
-                }
-            },
-            _ => {
-                println!("error loading image. this tool works with valid jpeg images.");
-                None
-            }
-        };
-
-        if let Some(image) = rotated_image {
+        if let Some(image) = self.perform_rotation(orientation) {
             self.save(image);
         }
 
         Ok(())
     }
 
+    /// Performs rotation based on the orientation value
+    fn perform_rotation(&self, orientation: u16) -> Option<DynamicImage> {
+        let bufreader = std::io::BufReader::new(&self.file);
+
+        match image::load(bufreader, ImageFormat::Jpeg) {
+            Ok(image) => match orientation {
+                1 | 2 | 4 | 5 | 7 => {
+                    println!("{}: orientation looks cool already!", self.path);
+                    None
+                }
+                8 => {
+                    println!("{}: rotating 270 degrees ...", self.path);
+                    Some(image.rotate270())
+                }
+                3 => {
+                    println!("{}: rotating 180 degrees ...", self.path);
+                    Some(image.rotate180())
+                }
+                6 => {
+                    println!("{}: rotating 90 degrees ...", self.path);
+                    Some(image.rotate90())
+                }
+                _ => {
+                    println!("{}: invalid orientation found :(", self.path);
+                    None
+                }
+            },
+            _ => {
+                println!(
+                    "{}: error loading image. this tool works with valid jpeg images.",
+                    self.path
+                );
+                None
+            }
+        }
+    }
+
+    /// Saves the rotated image to the same file
     fn save(&self, image: DynamicImage) {
         println!("image buf starts with {:?}", &image.to_bytes()[..10]);
 
